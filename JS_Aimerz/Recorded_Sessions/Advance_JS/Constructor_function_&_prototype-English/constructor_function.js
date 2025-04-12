dea609912bfa08197fced323cd742e4e5355150f
Amit Raj <rajamitnine@gmail.com>
2025-04-12 23:16:26 +0530

    Create a constructor function and instances of it.

--- JS_Aimerz/Recorded_Sessions/Advance_JS/Constructor_function_&_prototype-English/constructor_function.js
+++ JS_Aimerz/Recorded_Sessions/Advance_JS/Constructor_function_&_prototype-English/constructor_function.js
@@ -1,15 +1,15 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-  this.sayHello = function () {
-    console.log(`Hello Mr.${name}.`);
-  };
-}
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//   this.sayHello = function () {
+//     console.log(`Hello Mr.${name}.`);
+//   };
+// }
 
 // Instance 1 of constructor function :
-const amit = new Person("Amit", 28);
-console.log(amit);
-amit.sayHello();
+// const amit = new Person("Amit", 28);
+// console.log(amit);
+// amit.sayHello();
 
 // Instance 2 of constructor function :
 // const monu = new Person("Monu", 26);
@@ -18,19 +18,42 @@ amit.sayHello();
 
 // ----------------------------------------------------------------
 
-function Animal(name, type) {
-  this.name = name;
-  this.type = type;
-  this.animalDetail = function () {
-    console.log(`I am a ${name}.`);
+// function Animal(name, type) {
+//   this.name = name;
+//   this.type = type;
+//   this.animalDetail = function () {
+//     console.log(`I am a ${name}.`);
+//   };
+// }
+
+// Instance 1 of constructor function(Animal) :
+// const dog = new Animal("dog", "pet");
+// console.log(dog);
+// dog.animalDetail();
+
+// Instance 2 of constructor function(Animal) :
+// const cat = new Animal("cat", "wild");
+// console.log(cat);
+// cat.animalDetail();
+
+// ---------------------------------------------------------------------------------------
+
+function Car(model, brand) {
+  this.model = model;
+  this.brand = brand;
+  this.carDetails = function () {
+    console.log(`I drive ${brand} ${model}.`);
   };
 }
 
-// Instance 1 of constructor function(Animal) :
-const dog = new Animal("dog", "pet");
-console.log(dog);
-dog.animalDetail();
+// instance-1 of constructor function (Car) :
+const bmw = new Car("7-series", "BMW");
+console.log(bmw);
+bmw.carDetails();
 
-const cat = new Animal("cat", "wild");
-console.log(cat);
-cat.animalDetail();
+// instance-2 of constructor function (Car) :
+const tata = new Car("Tiago", "Tata");
+console.log(tata);
+tata.carDetails();
+
+// -----------------------------------------------------------------------------------
