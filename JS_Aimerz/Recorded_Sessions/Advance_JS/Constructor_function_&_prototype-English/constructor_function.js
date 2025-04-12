7864138a4e895b35f9a321ee7833018e78671609
Amit Raj <rajamitnine@gmail.com>
2025-04-12 23:08:20 +0530

    Create a constructor function 'Animal' with two instances(objects) of it, namely dog and cat.

--- JS_Aimerz/Recorded_Sessions/Advance_JS/Constructor_function_&_prototype-English/constructor_function.js
+++ JS_Aimerz/Recorded_Sessions/Advance_JS/Constructor_function_&_prototype-English/constructor_function.js
@@ -5,12 +5,32 @@ function Person(name, age) {
     console.log(`Hello Mr.${name}.`);
   };
 }
+
 // Instance 1 of constructor function :
 const amit = new Person("Amit", 28);
 console.log(amit);
 amit.sayHello();
 
 // Instance 2 of constructor function :
-const monu = new Person("Monu", 26);
-console.log(monu);
-monu.sayHello();
+// const monu = new Person("Monu", 26);
+// console.log(monu);
+// monu.sayHello();
+
+// ----------------------------------------------------------------
+
+function Animal(name, type) {
+  this.name = name;
+  this.type = type;
+  this.animalDetail = function () {
+    console.log(`I am a ${name}.`);
+  };
+}
+
+// Instance 1 of constructor function(Animal) :
+const dog = new Animal("dog", "pet");
+console.log(dog);
+dog.animalDetail();
+
+const cat = new Animal("cat", "wild");
+console.log(cat);
+cat.animalDetail();
