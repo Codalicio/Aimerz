49e2df0a77fc407c351d77d659294c99bec7b332
Amit Raj <rajamitnine@gmail.com>
2025-04-13 00:24:10 +0530

    Add a constructor function, create an instance of it, pass the properties of the instance created first to the last created instance using Object.create(<instanceName>), and the instance created lastly will have all the properties inherited into it's __proto__.

--- JS_Aimerz/Recorded_Sessions/Advance_JS/Constructor_function_&_prototype-English/prototype_&_proto.js
+++ JS_Aimerz/Recorded_Sessions/Advance_JS/Constructor_function_&_prototype-English/prototype_&_proto.js
@@ -1,8 +1,43 @@
-function Person(name, age) {
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//   this.sayHi = function () {
+//     console.log(`Hi, ${this.name}.`);
+//   };
+// }
+
+// function sayBye() {
+//   console.log(`Bye, ${this.name}.`);
+// }
+
+// console.log(Person.prototype);
+// Person.prototype.sayBye = sayBye;
+// console.log(Person.prototype);
+
+// const amit = new Person("Amit", 28);
+// console.log(amit);
+// console.log(amit.__proto__);
+// amit.sayHi();
+// amit.sayBye();
+
+// console.log(Person.prototype === amit.__proto__);
+
+// Prototype Inheritance :
+
+// const Student = Object.create(amit);
+// console.log(Student);
+// console.log(Student.__proto__);
+// Student.name = "Sonu";
+// console.log(Student.__proto__);
+// Student.sayHi();
+// Student.sayBye();
+
+// --------------------------------------------------------------------
+
+function Person(name) {
   this.name = name;
-  this.age = age;
   this.sayHi = function () {
-    console.log(`Hi, ${name}.`);
+    console.log(`Hi, ${this.name}.`);
   };
 }
 
@@ -11,13 +46,21 @@ function sayBye() {
 }
 
 console.log(Person.prototype);
+
 Person.prototype.sayBye = sayBye;
+
 console.log(Person.prototype);
 
-const amit = new Person("Amit", 28);
-console.log(amit);
-console.log(amit.__proto__);
-amit.sayHi();
-amit.sayBye();
+const monu = new Person("Monu");
+console.log(monu);
+console.log(monu.__proto__);
+monu.sayHi();
+monu.sayBye();
 
-console.log(Person.prototype === amit.__proto__);
+const Student = Object.create(monu);
+Student.name = "Prabir";
+console.log(Student.__proto__);
+Student.sayHi();
+Student.sayBye();
+
+// ---------------------------------------------------------------------
