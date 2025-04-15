4dd9835fd2e556c68d06a28140750dbfe28d76b8
Amit Raj <rajamitnine@gmail.com>
2025-04-15 14:31:46 +0530

    Use call method with parameters, in this case pass two values for call method, first one will do the this binding and second will pass the argument for the underlying method.

--- JS_Aimerz/Live_Sessions/16_DEC_2024/call.js
+++ JS_Aimerz/Live_Sessions/16_DEC_2024/call.js
@@ -1,19 +1,39 @@
 // call method without parameters :
 
+// const person1 = {
+//   name: "Raj",
+//   age: 34,
+//   greet: function () {
+//     console.log(this.name);
+//   },
+// };
+
+// const person2 = {
+//   name: "Amit",
+//   age: 27,
+// };
+
+// person1.greet();
+// person1.greet.call(person2);
+
+// ----------------------------------------------------------------------------------
+
+// call method with parameters :
+
 const person1 = {
-  name: "Raj",
-  age: 34,
-  greet: function () {
-    console.log(this.name);
+  name: "Amit",
+  age: 28,
+  greet: function (greeting) {
+    console.log(`${greeting}, ${this.name}.`);
   },
 };
 
 const person2 = {
-  name: "Amit",
-  age: 27,
+  name: "Sonu",
+  age: 32,
 };
 
-person1.greet();
-person1.greet.call(person2);
+person1.greet("Hello");
+person1.greet.call(person2, "Good Morning");
 
-// ----------------------------------------------------------------------------------
+// -------------------------------------------------------------------------
