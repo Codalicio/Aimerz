67e51caa06c09386e862ca2f66b31bb61f9dbdf1
Amit Raj <rajamitnine@gmail.com>
2025-04-15 21:16:37 +0530

    Explain the working of OOPs concepts in JavaScript through classes and objects

---
+++ JS_Aimerz/Live_Sessions/21_DEC_2024/oops.js
@@ -0,0 +1,16 @@
+// OOPs : OOPs stands for 'Object Oriented Programming System'.
+
+// OOJS : Object Oriented JavaScript :
+
+// In OOJS, we use classes and object :
+// * Classes are the syntactical sugar of the constructor function.
+// * And Instances are referred to as objects here in OOJS.
+
+// OOPs has mainly four pillars :
+// 1. Encapsulation : Can be achieved using the : constructor()
+
+// 2. Abstraction : Can be achieved using getters and setters, getters and setters are not methods they are properties of the classes and we can achieve abstraction by defining checks or requirements inside the setters property of the class.
+
+// 3. Inheritance : Can be achieved by using extends keyword and super(). Suppose, there is a class 'Person' with name, age and place initialized inside its constructor function and we want to create an another class 'Student' that also has the same properties like name, age, place, with one exception, i.e., grade. So to avoid code duplication and also Student can inherit the properties of Person we will use the 'extends' keyword and use the class Person as Parent(just in Context) and inherit the similar properties using the super() method inside the constructor of the Student class and thus, it will avoid code duplication and also the methods of the parent(Person) will be made available to the child(Student) for use.
+
+// 4. Polymorphism : Same thing with different forms : We can achieve this by defining the same method inherited from the parent(Person) inside the child(Student) class with the desired modifications.
