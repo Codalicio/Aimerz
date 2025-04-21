907d29869e34361d4171ae1c54d269beaf0e544b
Amit Raj <rajamitnine@gmail.com>
2025-04-21 16:39:14 +0530

    Solve Question-2

---
+++ JS_Aimerz/Practice_EBook/Numbers/Question-2/file1.js
@@ -0,0 +1,27 @@
+// Question : Round a number to the nearest integer.
+// - Write a function that rounds a number to the nearest whole number.
+
+// function roundNumber(num) {
+//   return Math.round(num);
+// }
+
+// console.log(roundNumber(4.3));
+// console.log(roundNumber(4.7));
+
+// -------------------------------------------------------------------------
+
+// const num = 5.5;
+// const result = Math.round(num);
+// console.log(result);
+
+// ------------------------------------------------------------------------
+
+const roundNumber = function (num) {
+  return Math.round(num);
+};
+
+console.log(roundNumber(4.3));
+console.log(roundNumber(4.5));
+console.log(roundNumber(4.9));
+
+// ----------------------------------------------------------------------
