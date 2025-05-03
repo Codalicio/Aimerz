7a77af20b9ed56267f89f1044e22b857fcb58856
Amit Raj <rajamitnine@gmail.com>
2025-05-03 20:05:34 +0530

    Implement component styling using css modules and frameworks like tailwindcss

---
+++ React_Aimerz/Recorded_Sessions/Component_Styling/Styles_in_React-English/component-styling/src/App.jsx
@@ -0,0 +1,15 @@
+import styles from "./css module/styles.module.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-[80%] mx-auto my-15 border-7 border-green-700 rounded shadow-xl/30">
+        <h1 className="bg-red-600 p-5 text-white font-bold text-4xl text-center">
+          Component Styling
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
